5ef3471176870071b0ad6ab1ecac9407fac0d6df
Script47 <54595497+Script47ph@users.noreply.github.com>
2024-04-07 20:14:45 +0700

    Update

--- scripts/generate-network.sh
+++ scripts/generate-network.sh
@@ -1,50 +1,50 @@
 generate_net() {
     cat <<EOF> $TMPDIR/cluster-network.tf
     provider "libvirt" {
-        uri = "qemu:///system"
+      uri = "qemu:///system"
     }
 
     terraform {
-    required_version = ">= 0.13"
-    required_providers {
+      required_version = ">= 0.13"
+      required_providers {
         libvirt = {
-        source  = "dmacvicar/libvirt"
-        version = "0.7.1"
+          source  = "dmacvicar/libvirt"
+          version = "0.7.1"
         }
-    }
+      }
     }
 
     // Network Section
     resource "libvirt_network" "eth0" {
-        name = "$ETH0_NAME"
-        mode = "$NETWORK_MODE"
-        bridge = "$ETH0_BR"
-        addresses = ["$ETH0_ADDRESSES"]
-        dhcp {
-            enabled = true
-        }
+      name = "$ETH0_NAME"
+      mode = "$NETWORK_MODE"
+      bridge = "$ETH0_BR"
+      addresses = ["$ETH0_ADDRESSES"]
+      dhcp {
+        enabled = true
+      }
     }
 
     // Network Section
     resource "libvirt_network" "eth1" {
-        name = "$ETH1_NAME"
-        mode = "$NETWORK_MODE"
-        bridge = "$ETH1_BR"
-        addresses = ["$ETH1_ADDRESSES"]
-        dhcp {
-            enabled = true
-        }
+      name = "$ETH1_NAME"
+      mode = "$NETWORK_MODE"
+      bridge = "$ETH1_BR"
+      addresses = ["$ETH1_ADDRESSES"]
+      dhcp {
+        enabled = true
+      }
     }
 
     // Network Section
     resource "libvirt_network" "eth2" {
-        name = "$ETH2_NAME"
-        mode = "$NETWORK_MODE"
-        bridge = "$ETH2_BR"
-        addresses = ["$ETH2_ADDRESSES"]
-        dhcp {
-            enabled = true
-        }
+      name = "$ETH2_NAME"
+      mode = "$NETWORK_MODE"
+      bridge = "$ETH2_BR"
+      addresses = ["$ETH2_ADDRESSES"]
+      dhcp {
+        enabled = true
+      }
     }
 EOF
 }
