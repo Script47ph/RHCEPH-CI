eb034ac2a21921a96ba35b5cf21354971544a062
Script47 <54595497+Script47ph@users.noreply.github.com>
2024-04-07 16:48:25 +0700

    Create generate-network.sh

---
+++ scripts/generate-network.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+cat <<EOF> cluster-network.tf
+provider "libvirt" {
+    uri = "qemu:///system"
+}
+
+terraform {
+ required_version = ">= 0.13"
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.7.1"
+    }
+  }
+}
+
+// Network Section
+resource "libvirt_network" "eth0" {
+    name = "${{ vars.ETH0_NAME }}"
+    mode = "${{ vars.NETWORK_MODE }}"
+    bridge = "${{ vars.BR_ETH0 }}"
+    addresses = ["${{ vars.ETH0_ADDRESSES }}"]
+    dhcp {
+        enabled = true
+    }
+}
+EOF
