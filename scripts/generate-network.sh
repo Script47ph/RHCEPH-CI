b70de39bda42c261e440e69e8cb3d58fd10f6e4d
Script47 <54595497+Script47ph@users.noreply.github.com>
2024-04-07 17:08:57 +0700

    Update generate-network.sh

--- scripts/generate-network.sh
+++ scripts/generate-network.sh
@@ -1,27 +1,26 @@
-# cat <<EOF> cluster-network.tf
-# provider "libvirt" {
-#     uri = "qemu:///system"
-# }
+cat <<EOF> cluster-network.tf
+provider "libvirt" {
+    uri = "qemu:///system"
+}
 
-# terraform {
-#  required_version = ">= 0.13"
-#   required_providers {
-#     libvirt = {
-#       source  = "dmacvicar/libvirt"
-#       version = "0.7.1"
-#     }
-#   }
-# }
+terraform {
+ required_version = ">= 0.13"
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.7.1"
+    }
+  }
+}
 
-# // Network Section
-# resource "libvirt_network" "eth0" {
-#     name = "${{ vars.ETH0_NAME }}"
-#     mode = "${{ vars.NETWORK_MODE }}"
-#     bridge = "${{ vars.BR_ETH0 }}"
-#     addresses = ["${{ vars.ETH0_ADDRESSES }}"]
-#     dhcp {
-#         enabled = true
-#     }
-# }
-# EOF
-echo $ETH0_NAME
+// Network Section
+resource "libvirt_network" "eth0" {
+    name = "$ETH0_NAME"
+    mode = "$NETWORK_MODE"
+    bridge = "$BR_ETH0"
+    addresses = ["$ETH0_ADDRESSES"]
+    dhcp {
+        enabled = true
+    }
+}
+EOF
